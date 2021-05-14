4e9872d61554e855852de077c1663dd825a45bda
Victor Adeleke AFOLAYAN <adelekeafolayan@gmail.com>
2021-05-14 23:31:46 +0100

    completed task

--- src/index.js
+++ src/index.js
@@ -1,4 +1,4 @@
-var numbers = [3, 56, 2, 48, 5];
+// var numbers = [3, 56, 2, 48, 5];
 
 //Map -Create a new array by doing something with each item in an array.
 
@@ -9,3 +9,18 @@ var numbers = [3, 56, 2, 48, 5];
 //Find - find the first item that matches from an array.
 
 //FindIndex - find the index of the first item that matches.
+import emojipedia from "./emojipedia";
+
+const meaning = emojipedia.map(function (dummy) {
+  return dummy.meaning.slice(0, 100);
+});
+
+console.log(meaning);
+
+// function truncate(words) {
+//   return words.slice(0, 100).concat("...");
+// }
+
+// const final = emojipedia.meaning.filter(trucate);
+
+// console.log(final);
